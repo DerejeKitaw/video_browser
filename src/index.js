b16bd9826b5af6d973d9c81fcbb4999a7a37ac62
Francisco De La Cruz <helloquico@gmail.com>
2016-01-09 00:32:00 -0500

    Throttle Search Term

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 import API_KEY from './key';
 
 class App extends Component {
@@ -28,9 +29,11 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term); }, 300);
+
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
